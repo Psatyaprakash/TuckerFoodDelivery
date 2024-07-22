d593884112311614fdbc6024f8cf796938cb5f9e
Psatyaprakash <125766030+Psatyaprakash@users.noreply.github.com>
2024-07-22 16:15:32 +0530

    Start

---
+++ app/src/main/java/com/example/tuckerfooddelivery/Model/API/ApiService.kt
@@ -0,0 +1,10 @@
+package com.example.tuckerfooddelivery.Model.API
+
+//import com.example.tuckerfooddelivery.Model.Data.
+//import retrofit2.http.GET
+//
+//interface ApiService{
+//    @GET("")
+//    suspend fun getItems(): List<>
+//
+//}
