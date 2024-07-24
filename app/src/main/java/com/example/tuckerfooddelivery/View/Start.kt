837fd2940070822154ca52758852523770ec2d9f
Psatyaprakash <125766030+Psatyaprakash@users.noreply.github.com>
2024-07-24 20:25:56 +0530

    First 3 Landing pages

--- app/src/main/java/com/example/tuckerfooddelivery/View/Start.kt
+++ app/src/main/java/com/example/tuckerfooddelivery/View/Start.kt
@@ -1,5 +1,7 @@
 package com.example.tuckerfooddelivery.View
 
+import androidx.compose.animation.expandIn
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -18,6 +20,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -30,15 +33,17 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
 import com.example.tuckerfooddelivery.R
 
 @Composable
-fun Start(){
+fun Start(navController: NavHostController) {
     val Mustard_yellow = colorResource(id = R.color.Mustard_yellow)
     val Mustard_yellow_light = colorResource(id = R.color.Mustard_yellow_light)
     Box(modifier = Modifier
         .fillMaxSize()
-        .background(Color.White)
+        .background(Color.White),
+//        slideOutHorizontally {  }
     ){
         Column(modifier = Modifier
             .padding(40.dp)
@@ -79,7 +84,7 @@ fun Start(){
                 }
             }
             Spacer(modifier = Modifier .height(10.dp) )
-            Button(onClick = { /*TODO*/ },
+            Button(onClick = { navController.navigate("Start2") },
                 Modifier
                     .fillMaxWidth()
                     .padding(30.dp)
@@ -89,7 +94,139 @@ fun Start(){
             ) {
                 Text(text = "Next" , fontSize = 22.sp)
             }
-            Text(text = "Skip" , fontSize = 25.sp , fontFamily = FontFamily.SansSerif)
+            TextButton(onClick = { navController.navigate("Login") }) {
+                Text(text = "Skip", fontSize = 25.sp, fontFamily = FontFamily.SansSerif , color = Color.Black)
+            }
+            Spacer(modifier = Modifier.height(50.dp))
+        }
+    }
+}
+
+
+@Composable
+fun Start2(navController: NavHostController) {
+    val Mustard_yellow = colorResource(id = R.color.Mustard_yellow)
+    val Mustard_yellow_light = colorResource(id = R.color.Mustard_yellow_light)
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(Color.White)
+    ){
+        Column(modifier = Modifier
+            .padding(40.dp)
+            .fillMaxSize()
+            .background(Color.White),
+            verticalArrangement = Arrangement.Bottom,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(painter = painterResource(R.drawable.order),
+                contentDescription = "" ,
+                Modifier
+                    .size(500.dp)
+                )
+            Text(text = "Order from chosen chef" , fontSize = 26.sp, fontWeight = FontWeight.Bold)
+            Spacer(modifier = Modifier .height(20.dp) )
+            Text(text = "Get all your loved foods in one once place,\n" +
+                    "you just place the order we do the rest" ,
+                fontSize = 18.sp,
+                textAlign = TextAlign.Center ,
+                color = Color.Gray ,
+                fontWeight = FontWeight.W700
+            )
+            Spacer(modifier = Modifier .height(100.dp) )
+            Row{
+                for (i in 1..3) {
+                    var col_mus = Mustard_yellow_light
+
+                    if (i == 2)
+                        col_mus = Mustard_yellow
+
+                    Box(
+                        modifier = Modifier
+                            .size(10.dp)
+                            .clip(CircleShape)
+                            .background(col_mus)
+                    )
+                    Spacer(modifier = Modifier.width(5.dp))
+                }
+            }
+            Spacer(modifier = Modifier .height(10.dp) )
+            Button(onClick = { navController.navigate("Start3") },
+                Modifier
+                    .fillMaxWidth()
+                    .padding(30.dp)
+                    .height(60.dp),
+                shape = RoundedCornerShape(10.dp),
+                colors = ButtonDefaults.buttonColors(Mustard_yellow)
+            ) {
+                Text(text = "Next" , fontSize = 22.sp)
+            }
+            TextButton(onClick = { navController.navigate("Login") }) {
+                Text(text = "Skip", fontSize = 25.sp, fontFamily = FontFamily.SansSerif , color = Color.Black)
+            }
+            Spacer(modifier = Modifier.height(50.dp))
+        }
+    }
+}
+
+
+@Composable
+fun Start3(navController: NavHostController) {
+    val Mustard_yellow = colorResource(id = R.color.Mustard_yellow)
+    val Mustard_yellow_light = colorResource(id = R.color.Mustard_yellow_light)
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(Color.White)
+    ){
+        Column(modifier = Modifier
+            .padding(40.dp)
+            .fillMaxSize()
+            .background(Color.White),
+            verticalArrangement = Arrangement.Bottom,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(painter = painterResource(R.drawable.delivery),
+                contentDescription = "" ,
+                Modifier
+                    .size(500.dp)
+                )
+            Text(text = "Free delivery offers" , fontSize = 26.sp, fontWeight = FontWeight.Bold)
+            Spacer(modifier = Modifier .height(20.dp) )
+            Text(text = "Get all your loved foods in one once place,\n" +
+                    "you just place the order we do the rest" ,
+                fontSize = 18.sp,
+                textAlign = TextAlign.Center ,
+                color = Color.Gray ,
+                fontWeight = FontWeight.W700
+            )
+            Spacer(modifier = Modifier .height(100.dp) )
+            Row{
+                for (i in 1..3) {
+                    var col_mus = Mustard_yellow_light
+
+                    if (i == 3)
+                        col_mus = Mustard_yellow
+
+                    Box(
+                        modifier = Modifier
+                            .size(10.dp)
+                            .clip(CircleShape)
+                            .background(col_mus)
+                    )
+                    Spacer(modifier = Modifier.width(5.dp))
+                }
+            }
+            Spacer(modifier = Modifier .height(10.dp) )
+            Button(onClick = { navController.navigate("Login") },
+                Modifier
+                    .fillMaxWidth()
+                    .padding(30.dp)
+                    .height(60.dp),
+                shape = RoundedCornerShape(10.dp),
+                colors = ButtonDefaults.buttonColors(Mustard_yellow)
+            ) {
+                Text(text = "Get Started" , fontSize = 22.sp)
+            }
+
             Spacer(modifier = Modifier.height(50.dp))
         }
     }
