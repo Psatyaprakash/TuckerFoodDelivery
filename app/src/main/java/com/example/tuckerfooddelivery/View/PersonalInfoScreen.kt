e0126e50ee8a9ed2a21171feb55d49d6fb741145
Avipsa-Biswal <cse.22bcse37@silicon.ac.in>
2024-07-25 02:24:15 +0530

    Tucker Food Delivery App

--- app/src/main/java/com/example/tuckerfooddelivery/View/PersonalInfoScreen.kt
+++ app/src/main/java/com/example/tuckerfooddelivery/View/PersonalInfoScreen.kt
@@ -1,9 +1,13 @@
 package com.example.tuckerfooddelivery.View
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
@@ -16,9 +20,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.rememberImagePainter
 
 @Composable
 fun CircularButtonWithSymbol(onClick: () -> Unit) {
@@ -65,17 +72,43 @@ fun PersonalInfoDetails() {
             fontSize = 24.sp,
             modifier = Modifier
                 .align(Alignment.TopCenter)
-                .padding(top = 24.dp) // Adjust spacing if needed
+                .padding(top = 11.dp) // Adjust spacing if needed
         )
 
-        // Form Content
+        // Profile Image and Edit Button
         Column(
             modifier = Modifier
                 .align(Alignment.Center)
                 .fillMaxSize()
-                .padding(top = 120.dp)
+                .padding(top = 60.dp)
+                .align(Alignment.TopCenter),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(text = "FULL NAME", color = Color.Black, fontSize = 18.sp)
+            Image(
+                painter = rememberImagePainter(data = "https://www.bing.com/images/search?view=detailV2&ccid=7hFAXSqY&id=CC0399CC4062E603BD734E0EF136B2C10A280510&thid=OIP.7hFAXSqYekM7JbRZma2zUwHaEm&mediaurl=https%3a%2f%2fwww.pngitem.com%2fpimgs%2fm%2f159-1595932_python-logo-png-transparent-images-logo-transparent-background.png&exph=535&expw=860&q=google+python+logo&simid=608042017004537305&FORM=IRPRST&ck=E62F0753280B23CB03B0752D4B290D5B&selectedIndex=5&itb=0"),
+                contentDescription = "Profile Image",
+                modifier = Modifier
+                    .size(100.dp)
+                    .clip(CircleShape)
+                    .border(2.dp, Color.Gray, CircleShape)
+            )
+            Spacer(modifier = Modifier.height(5.dp))
+            TextButton(
+                onClick = { /* handle image edit click */ },
+            ) {
+                Text("Edit Image", color = Color.Blue)
+            }
+            Spacer(modifier = Modifier.height(5.dp))
+        }
+
+        // Form Content
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 200.dp) // Adjust the top padding to ensure space for the profile image
+                .verticalScroll(rememberScrollState())
+        ) {
+            Text(text = "FULL NAME", color = Color.Black, fontSize = 14.sp)
             Spacer(modifier = Modifier.height(10.dp))
             // Name
             TextField(
@@ -88,7 +121,7 @@ fun PersonalInfoDetails() {
             )
             Spacer(modifier = Modifier.height(30.dp))
             // Email
-            Text(text = "E-MAIL", color = Color.Black, fontSize = 18.sp)
+            Text(text = "E-MAIL", color = Color.Black, fontSize = 14.sp)
             Spacer(modifier = Modifier.height(10.dp))
             TextField(
                 value = email,
@@ -100,7 +133,7 @@ fun PersonalInfoDetails() {
             )
             Spacer(modifier = Modifier.height(30.dp))
             // Phone Number
-            Text(text = "PHONE NUMBER", color = Color.Black, fontSize = 18.sp)
+            Text(text = "PHONE NUMBER", color = Color.Black, fontSize = 14.sp)
             Spacer(modifier = Modifier.height(10.dp))
             TextField(
                 value = phoneNumber,
@@ -112,7 +145,7 @@ fun PersonalInfoDetails() {
             )
             Spacer(modifier = Modifier.height(30.dp))
             // Bio
-            Text(text = "BIO", color = Color.Black, fontSize = 18.sp)
+            Text(text = "BIO", color = Color.Black, fontSize = 14.sp)
             Spacer(modifier = Modifier.height(10.dp))
             TextField(
                 value = bio,
