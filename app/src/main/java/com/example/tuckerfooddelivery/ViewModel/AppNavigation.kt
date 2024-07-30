48f251882d343af677b3b45a638ef110825ff5ec
TanushreePandaa <tanupanda26@gmail.com>
2024-07-30 17:07:05 +0530

    item details

--- app/src/main/java/com/example/tuckerfooddelivery/ViewModel/AppNavigation.kt
+++ app/src/main/java/com/example/tuckerfooddelivery/ViewModel/AppNavigation.kt
@@ -8,17 +8,34 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.tuckerfooddelivery.View.AmericanCorn
+import com.example.tuckerfooddelivery.View.AmericanCornCart
+import com.example.tuckerfooddelivery.View.BlueLagoon
+import com.example.tuckerfooddelivery.View.BuffaloWings
+import com.example.tuckerfooddelivery.View.BurgerBistro
 import com.example.tuckerfooddelivery.View.Burger_Category
 import com.example.tuckerfooddelivery.View.Cart
+import com.example.tuckerfooddelivery.View.ChickenWings
+import com.example.tuckerfooddelivery.View.ClassicBurger
 import com.example.tuckerfooddelivery.View.ClassicFrenchFries
+import com.example.tuckerfooddelivery.View.CrispyChickenRoll
 import com.example.tuckerfooddelivery.View.HomePage
 import com.example.tuckerfooddelivery.View.LoginScreen
+import com.example.tuckerfooddelivery.View.ManchowSoup
+import com.example.tuckerfooddelivery.View.MargheritaCornPizza
+import com.example.tuckerfooddelivery.View.MexicanPepperoniPizza
+import com.example.tuckerfooddelivery.View.NepaliDumplingMomos
+import com.example.tuckerfooddelivery.View.OrangeMimosa
+import com.example.tuckerfooddelivery.View.PaneerRoll
+import com.example.tuckerfooddelivery.View.PeriPeriFries
 import com.example.tuckerfooddelivery.View.PizzaCalzone
 import com.example.tuckerfooddelivery.View.Rosegardenrestaurant_landingpage
 import com.example.tuckerfooddelivery.View.SkyHighW_landingpage
+import com.example.tuckerfooddelivery.View.SmokingBurger
 import com.example.tuckerfooddelivery.View.Start
 import com.example.tuckerfooddelivery.View.Start2
 import com.example.tuckerfooddelivery.View.Start3
+import com.example.tuckerfooddelivery.View.TibetanMomos
 import com.example.tuckerfooddelivery.View.fionah_landingpage
 
 //Navigation
@@ -37,7 +54,7 @@ fun AppNavigation() {
                 slideOutOfContainer(
                     AnimatedContentTransitionScope.SlideDirection.Left,
                     tween(200) )},
-        startDestination = "ClassicFrenchFries"
+        startDestination = "OrangeMimosa"
     ) {
         composable("Start") { Start(navController) }
         composable("Start2") { Start2(navController) }
@@ -48,6 +65,22 @@ fun AppNavigation() {
         composable("ClassicFrenchFries"){ ClassicFrenchFries(navController) }
         composable("Burger_Category"){ Burger_Category(navController) }
         composable("Cart"){ Cart(navController) }
+        composable("BurgerBistro"){ BurgerBistro(navController) }
+        composable("TibetanMomos"){ TibetanMomos(navController) }
+        composable("BlueLagoon"){ BlueLagoon(navController) }
+        composable("ChickenWings"){ ChickenWings(navController) }
+        composable("SmokingBurger"){ SmokingBurger(navController) }
+        composable("MexicanPepperoniPizza"){ MexicanPepperoniPizza(navController) }
+        composable("CrispyChickenRoll"){ CrispyChickenRoll(navController) }
+        composable("AmericanCorn"){ AmericanCorn(navController) }
+        composable("BuffaloWings"){ BuffaloWings(navController) }
+        composable("ManchowSoup"){ ManchowSoup(navController) }
+        composable("PaneerRoll"){ PaneerRoll(navController) }
+        composable("MargheritaCornPizza"){ MargheritaCornPizza(navController) }
+        composable("ClassicBurger"){ ClassicBurger(navController) }
+        composable("NepalianDumplingMomos"){ NepaliDumplingMomos(navController) }
+        composable("PeriPeriFries"){ PeriPeriFries(navController) }
+        composable("OrangeMimosa"){ OrangeMimosa(navController) }
 
         composable("Rosegardenrestaurant_landingpage"){ Rosegardenrestaurant_landingpage(navController) }
         composable("SkyHighW_landingpage"){ SkyHighW_landingpage(navController) }
